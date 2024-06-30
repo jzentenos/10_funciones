e5a5910c7b33c635576f18756bb933ac3bf4432b
jzentenos <jzentenos@unjbg.edu.pe>
2024-06-30 11:26:20 -0500

    Ejercicio 2:  Se agrego la condicion, que el numero MAX debe ser mayor a 0

--- num-aleatorio.cpp
+++ num-aleatorio.cpp
@@ -5,13 +5,17 @@ int main(){
 	int n,cant, alea;
 	
 	cout<<"Ingrese el valor MAX: "; cin>>n;
-	cout<<"Ingrese la cantidad de numeros a generar: "; cin>>cant;
-	cout<<"\tValores desde 0 hasta "<<n<<endl;
 	
-	for(int i=1; i<=cant; i++){
-		alea= 0 + rand()%(n+1);
-		cout<<i<<".- "<<alea<<endl;
+	if(n>0){
+		cout<<"Ingrese la cantidad de numeros a generar: "; cin>>cant;
+		cout<<"\tValores desde 0 hasta "<<n<<endl;
+		for(int i=1; i<=cant; i++){
+			alea= 0 + rand()%(n+1);
+			cout<<i<<".- "<<alea<<endl;
+		}		
 	}
-	
+	else{
+		cout<<"El valor MAX tiene que ser mayor a 0"<<endl;
+	}
 	return 0;
 }
