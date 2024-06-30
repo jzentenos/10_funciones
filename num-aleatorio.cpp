5f88e7e02f8c687f410f6202da2b817320838a9f
jzentenos <jzentenos@unjbg.edu.pe>
2024-06-30 11:37:31 -0500

    Ejercicio 2: Se agrego la biblioteca time.h para que la generacion de numeros sea diferente en cada ejecucion y la comprobacion de que la cantidad de numero a generar sea positiva.

--- num-aleatorio.cpp
+++ num-aleatorio.cpp
@@ -1,17 +1,23 @@
 #include<iostream>
 #include<cstdlib>
+#include<time.h>
 using namespace std;
 int main(){
 	int n,cant, alea;
-	
+	srand(time(NULL));
 	cout<<"Ingrese el valor MAX: "; cin>>n;
 	
 	if(n>0){
 		cout<<"Ingrese la cantidad de numeros a generar: "; cin>>cant;
-		cout<<"\tValores desde 0 hasta "<<n<<endl;
-		for(int i=1; i<=cant; i++){
+		if(cant>0){
+			cout<<"\tValores desde 0 hasta "<<n<<endl;
+			for(int i=1; i<=cant; i++){
 			alea= 0 + rand()%(n+1);
 			cout<<i<<".- "<<alea<<endl;
+			}
+		}
+		else{
+			cout<<"La cantidad de numeros a generar es INVALIDO"<<endl;
 		}		
 	}
 	else{
