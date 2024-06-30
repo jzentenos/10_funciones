a27b5479b22be6ce6d61776e4b18bca47c6d87b0
jzentenos <jzentenos@unjbg.edu.pe>
2024-06-30 11:00:00 -0500

    Ejercicio 1: Se agrego la comprobacion, si el radio es menor igual a 0 y la pausa en el sistema

--- areacirculo.cpp
+++ areacirculo.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<cmath>
+#include<stdlib.h>
 //Se define el valor de PI en Dev-C++
 #define M_PI 3.14159265358979323846
 using namespace std;
@@ -7,8 +8,15 @@ double AreaC(double);
 int main(){
     double a, r;
     cout<<"Ingrese el radio del circulo: "; cin>>r;
-    a= AreaC(r); // Llamdo a la funcion AreaC
-    cout<<"El area del circulo es: "<<a<<endl;
+    if(r<=0){
+    	cout<<"La circunferencia no existe"<<endl;
+    	system("pause");
+    }
+    else{
+    	a= AreaC(r); // Llamdo a la funcion AreaC
+   	 	cout<<"El area del circulo es: "<<a<<endl;
+   	 	system("pause");
+    }
     return 0;
 }
 //Funcion Area del circulo
