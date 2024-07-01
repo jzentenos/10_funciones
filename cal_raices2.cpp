10a5cd18bb9833d569a778f55597d1b46af62b2c
jzentenos <jzentenos@unjbg.edu.pe>
2024-06-30 20:40:23 -0500

    Ejercicio 5: Se mejoró el ejercicio 4, agregando pasos de parámetro por referencia.

---
+++ cal_raices2.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<cmath>
+#include<stdlib.h>
+using namespace std;
+void calcularRaices(float, float, float, float &, float &, bool &, bool &);
+int main(){
+	float a, b, c;
+	cout<<"\tEcuacion Cuadratica: aX^2 + bx + c = 0"<<endl;
+	cout<<"Ingrese el valor de a: "; cin>>a;
+	if(a==0){
+		cout<<"El valor de a no puede ser CERO"<<endl;
+		system("pause");
+	}
+	else{
+		cout<<"Ingrese el valor de b: "; cin>>b;
+		cout<<"Ingrese el valor de c: "; cin>>c;
+		system("cls");
+		cout<<"Ecuacion: ("<<a<<")x^2 + ("<<b<<")x + ("<<c<<") "<<endl;
+	float x1, x2;
+	bool sr, si;	
+		calcularRaices(a, b, c, x1, x2, sr, si);
+	//Verificar que tipo de raiz es	
+        if(sr){
+            if(si){
+                cout<<"\tRaices iguales"<<endl;
+                cout<<"Raiz 1: "<<x1<<endl;
+                cout<<"Raiz 2: "<<x1<<endl;
+            } 
+			else{
+                cout<< "\tRaices Diferentes" << endl;
+                cout<< "Raiz 1: " << x1 << endl;
+                cout<< "Raiz 2: " << x2 << endl;
+            }
+        } 
+		else{
+            cout<<"\tRaices Imaginarias"<<endl;
+            cout<< "NO TIENE SOLUCION EN LOS REALES"<<endl;
+        }
+		system("pause");
+	}
+	return 0;
+} 
+//sr: Son raices Reales, si: Son raices Iguales
+void calcularRaices(float a, float b, float c, float &x1, float &x2, bool &sr, bool &si){
+	float d;
+	d= pow(b, 2) - 4*a*c;
+	if(d>0){
+		sr=true;
+		si=false;	
+		x1= ( -b + sqrt(d) ) / (2*a);
+		x2= ( -b - sqrt(d) ) / (2*a);
+	}
+	else if(d==0){
+		sr=true;
+		si=true;
+		x1= ( -b ) / (2*a);
+		x2=x1;	
+	}
+	else{
+		sr=false;
+		si=false;
+	}
+	
+}
