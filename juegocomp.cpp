34024600c4f9bfdeddac4c68721e5107e5d0c673
jzentenos <jzentenos@unjbg.edu.pe>
2024-06-30 21:12:42 -0500

    Ejercicio 6: Se creo las funciones eleccion, PPT, ganador y la funcion principal.

---
+++ juegocomp.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstdlib>
+#include<time.h>
+using namespace std;
+//Funcion que elige un valor aleatorio
+void eleccion(int &comp);
+//Funcion que imprime la eleccion de la computadora
+void PPT(int elec);
+//Funcion que determina el resultado
+void ganador(int user, int comp);
+//Funcion principal
+int main(){
+	srand(time(NULL)); //Valores diferentes en cada ejecucion
+	int user, comp;
+	return 0;
+}
+void eleccion(int &comp){
+	comp= 1+ rand()% 3; // Numero entre 1 y 3 
+}
+void PPT(const int elec){
+	switch(elec){
+		case 1: 
+			cout<<"Piedra";
+			break;
+		case 2: 
+			cout<<"Papel";
+			break;
+		case 3: 
+			cout<<"Tijera";
+			break;
+	}
+	
+}
+void ganador(int user, int comp){
+	if(user==comp){
+		cout<<"EMPATE"<<endl;
+	}
+	if( (user==1 && comp==3) || (user==2 && comp==1) || (user==3 && comp==2)){
+		cout<"GANASTE";
+	}
+	else{
+		cout<<"PERDISTE";
+	}
+}
