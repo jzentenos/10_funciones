1774c3c22b4da8b42a52834853672c797a60b3c6
jzentenos <jzentenos@unjbg.edu.pe>
2024-06-30 19:38:42 -0500

    Ejercicio 3: Se agrego la validacion de valores positivos de los catetos, pausa en el sistema al finalizar el programa y se creo una funcion para la hipotenusa.

--- hipotenusa.cpp
+++ hipotenusa.cpp
@@ -2,12 +2,33 @@
 #include<cmath>
 #include<stdlib.h>
 using namespace std;
+double Hip(float, float );
 int main(){
-	float ca, co, hipo;
-	cout<<"Ingrese el cateto adyacente: "; cin>>ca;
-	cout<<"Ingrese el cateto opuesto: "; cin>>co;
-	hipo= sqrt( pow(ca,2) + pow(co, 2) );
-	cout<<"La hipotenusa es: "<<hipo<<endl;
-	system("pause");
+	float a, b;
+	double h=0;
+	cout<<"\tPrograma que calcula la Hipotenusa"<<endl;
+	cout<<"Ingrese el cateto adyacente: "; cin>>a;
+	if(a>0){
+		cout<<"Ingrese el cateto opuesto: "; cin>>b;
+		if(b>0){
+			h=Hip(a, b);
+			cout<<"La hipotenusa es: "<<h<<endl;
+			system("pause");
+		}
+		else{
+			cout<<"Cateto Opuesto invalido"<<endl;
+			system("pause");
+		}
+	}
+	else{
+		cout<<"Cateto Adyacente invalido"<<endl;
+		system("pause");
+	}
+
 	return 0;
+}
+double Hip(float ca,float co){
+	double hipo=0;
+	hipo= sqrt( pow(ca,2) + pow(co, 2) );
+	return hipo;
 }
