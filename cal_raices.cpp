434c6bca6292b7b19ec2fc78bd3328247900249e
jzentenos <jzentenos@unjbg.edu.pe>
2024-06-30 20:10:30 -0500

    Ejercicio 4: Se creó el programa que halla las soluciones en una ecuación cuadrática

---
+++ cal_raices.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+void Cal_rai(float, float, float);
+int main(){
+	float a, b, c;
+	cout<<"\tEcuacion Cuadratica: aX^2 + bx + c = 0"<<endl;
+	cout<<"Ingrese el valor de a: "; cin>>a;
+	cout<<"Ingrese el valor de b: "; cin>>b;
+	cout<<"Ingrese el valor de c: "; cin>>c,
+	Cal_rai(a, b, c);
+	return 0;
+} 
+void Cal_rai(float a, float b, float c){
+	float d, x1, x2;
+	d= pow(b, 2) - 4*a*c;
+	if(d>0){
+		cout<<"\tRaices Diferentes"<<endl;		
+		x1= ( -b + sqrt(d) ) / (2*a);
+		x2= ( -b - sqrt(d) ) / (2*a);
+		cout<<"Raiz 1: "<<x1<<endl;
+		cout<<"Raiz 2: "<<x2<<endl;	
+	}
+	else if(d==0){
+		cout<<"\tRaices iguales"<<endl;
+		x1= ( -b + sqrt(d) ) / (2*a);
+		cout<<"Raiz 1: "<<x1<<endl;
+		cout<<"Raiz 2: "<<x1<<endl;	
+	}
+	else{
+		cout<<"\tRaices Imaginarias"<<endl;
+		cout<<"NO TIENE SOLUCION EN LOS REALES"<<endl;
+	}
+	
+	
+}
